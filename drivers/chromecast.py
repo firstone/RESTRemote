55e1fd1dfc3e9fb4d783dfe0a1afc3188c8dbf28
firstone <firstone@erissoft.com>
2018-07-24 17:55:20 -0400

    Chromecast connection logic

--- drivers/chromecast.py
+++ drivers/chromecast.py
@@ -1,4 +1,5 @@
 import pychromecast
+from pychromecast.discovery import discover_chromecasts
 
 from drivers.base_driver import BaseDriver
 import utils
@@ -7,8 +8,9 @@ import utils
 class Chromecast(BaseDriver):
 
     PLAYLISTS_KEY_NAME = 'chromecastPlaylists'
-
     CAST_LIST = {}
+    CAST_CONNECT_TRIES = 1
+    FRIENDLY_NAME_OFFSET = 4
 
     def __init__(self, config, logger, use_numeric_key=False):
         super(Chromecast, self).__init__(config, logger, use_numeric_key)
@@ -34,17 +36,36 @@ class Chromecast(BaseDriver):
             attr()
         return ''
 
+    def is_connected(self):
+        self.connected = False if self.cast is None else self.cast.socket_client.is_connected
+
+        return super(Chromecast, self).is_connected()
+
     def connect(self):
         if self.cast is None:
             Chromecast.discoverDevices(None)
 
             self.cast = Chromecast.CAST_LIST.get(self.config['name'])
 
-        if self.cast:
-            if not self.cast.socket_client.is_connected:
-                self.cast.socket_client.initialize_connection()
+            if not self.cast:
+                self.logger.debug('Host not found for %s. Discovering',
+                    self.config['name'])
+                hosts = discover_chromecasts()
+                for host in hosts:
+                    if host[Chromecast.FRIENDLY_NAME_OFFSET] == self.config['name']:
+                        self.logger.debug('Host found')
+                        self.cast = pychromecast._get_chromecast_from_host(host,
+                            Chromecast.CAST_CONNECT_TRIES)
+                        Chromecast.CAST_LIST[self.config['name']] = self.cast
+                        break
 
+        self.connected = False
+        if self.cast:
             self.connected = self.cast.socket_client.is_connected
+            if not self.connected:
+                self.cast.disconnect()
+                self.cast = None
+                Chromecast.CAST_LIST[self.config['name']] = None
 
     @staticmethod
     def processParams(config, param):
@@ -68,7 +89,7 @@ class Chromecast(BaseDriver):
     @staticmethod
     def discoverDevices(config):
         if len(Chromecast.CAST_LIST) == 0:
-            casts = pychromecast.get_chromecasts()
+            casts = pychromecast.get_chromecasts(Chromecast.CAST_CONNECT_TRIES)
 
             for cast in casts:
                 friendly_name = cast.device.friendly_name
